6d588ea00660916c4f96cb3df8f6b0b74d5b028a
Valentin Panait <valentin.panait@gmail.com>
2022-05-06 00:12:20 +0300

    fix to make prj run

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,38 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import { FeaturedBenefits } from '../components/sections/FeaturedBenefits'
-import { FeaturedPartners } from '../components/sections/FeaturedPartners/FeaturedPartners'
-import { GlobalFooter } from '../components/sections/GlobalFooter'
-import { GlobalNavigation } from '../components/sections/GlobalNavigation'
-import { HomeHero } from '../components/sections/HomeHero'
+import { FeaturedBenefits } from "../components/sections/FeaturedBenefits";
+import { FeaturedPartners } from "../components/sections/FeaturedPartners/FeaturedPartners";
+import { GlobalFooter } from "../components/sections/GlobalFooter";
+import { GlobalNavigation } from "../components/sections/GlobalNavigation";
+import { HomeHero } from "../components/sections/HomeHero";
 
-import featuredPartnersJSON from "../content/featured-partners.json"
-
+import featuredPartnersJSON from "../content/featured-partners.json";
 
 export function getStaticProps() {
-
+  return { props: {} };
 }
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Mula | The reporting tools taking the fintech world by storm</title>
-        <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam…" />
+        <title>
+          Mula | The reporting tools taking the fintech world by storm
+        </title>
+        <meta
+          name="description"
+          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam…"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <GlobalNavigation />
       <div className="grow">
         <HomeHero />
-        <FeaturedBenefits padding="mdTopOnly"  />
+        <FeaturedBenefits padding="mdTopOnly" />
         <FeaturedPartners partners={featuredPartnersJSON} />
       </div>
       <GlobalFooter />
     </>
-  )
+  );
 }
